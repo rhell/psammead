322dd717236c5fda13f89d859cd10a01ba547783
Ben Newcombe <bcmn@users.noreply.github.com>
2019-02-18 17:08:25 +0000

    Tidy colours.js file
    Make separation between V1.0 colours & other UX colours clearer, following feedback from Ngan-Thi

--- packages/utilities/psammead-styles/src/colours.js
+++ packages/utilities/psammead-styles/src/colours.js
@@ -1,8 +1,15 @@
 // BBC News Article colours
 export const C_EBON = '#222222';
 export const C_POSTBOX = '#B80000';
-export const C_STORM = '#404040';
+export const C_SHADOW = '#3F3F42';
+export const C_CLOUD_DARK = '#757575';
+export const C_CLOUD_LIGHT = '#BABABA';
+export const C_LUNAR = '#F2F2F2';
+export const C_GHOST = '#FDFDFD';
 export const C_WHITE = '#FFFFFF';
+
+// Other UX Colours (not currently used by articles)
+export const C_STORM = '#404040';
 export const C_BLUEJAY = '#0F556C';
 export const C_BLUEJAY_LHT = '#C3DEE7';
 export const C_OAT_LHT = '#F5F3F1';
@@ -10,11 +17,6 @@ export const C_PEBBLE = '#5C5752';
 export const C_RHINO = '#5A5A5A';
 export const C_STONE = '#D5D0CD';
 export const C_CHALK = '#ECEAE7';
-export const C_SHADOW = '#3F3F42';
-export const C_CLOUD_DARK = '#757575';
-export const C_CLOUD_LIGHT = '#BABABA';
-export const C_LUNAR = '#F2F2F2';
-export const C_GHOST = '#FDFDFD';
 
 // Colours from other BBC services
 export const C_ORBIT_GREY = '#4C4C4C';
